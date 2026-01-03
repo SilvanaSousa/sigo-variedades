8836bfed153a6feab02d2bdd25de4fb5f43269eb
SilvanaSousa <silvana.cdp15@gmail.com>
2026-01-03 16:17:58 -0300

    feat: add initial testing setup including base TestCase, application creation trait, and an example feature test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,14 +2,9 @@
 
 namespace Tests;
 
-use Tests\Feature\ExampleTest;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-    public function test_the_application_returns_a_successful_response()
-    {
-        $this->get('/')->assertStatus(200);
-    }
 }
